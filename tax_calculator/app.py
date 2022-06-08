f3ba44f1299d35faca30369da4bcf1c21576fe94
prica277 <prica277@hsd.k12.or.us>
2022-06-08 11:50:13 -0700

    Adding widgets

--- tax_calculator/app.py
+++ tax_calculator/app.py
@@ -1,1 +1,18 @@
 # app.py - My DearPyGui windowed app.
+import dearpygui.dearpygui as dpg
+
+dpg.create_context()
+dpg.create_viewport(title='Sales Tax Calculator', width=600, height=300)
+
+with dpg.window(label="Example Window": width=600, height=300):
+    dpg.add_text("Welcome to the Sales Tax Calculator")
+    dpg.add_input_float(label="Tax Rate")
+    dpg.add_input_float(label="Price")
+    dpg.add_button(label="Calculate")
+    dpg.add_button(label="Clear")
+
+
+dpg.setup_dearpygui()
+dpg.show_viewport()
+dpg.start_dearpygui()
+dpg.destroy_context()
