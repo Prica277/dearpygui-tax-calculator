6ce5cbd9acdebd547ef2e1176d4e3596a18cfdbb
prica277 <prica277@hsd.k12.or.us>
2022-06-10 11:09:11 -0700

    added rounding for rate

--- tax_calculator/app.py
+++ tax_calculator/app.py
@@ -7,11 +7,14 @@ price_id = dpg.generate_uuid()
 output_id = dpg.generate_uuid()
 
 def callback():
-    print("Rate ID: " + str(dpg.get_value(rate_id)))
+    rate = dpg.get_value(rate_id)
+    rate = round(rate, 3)
+    output= "Rate ID: " + str(rate)
+    dpg.set_value(output_id, output)
 
 dpg.create_viewport(title='Sales Tax Calculator', width=600, height=300)
 
-with dpg.window(label="Example Window", width=600, height=300) :
+with dpg.window(label="Sales Tax Calculator", width=600, height=300) :
     dpg.add_text("Welcome to the Sales Tax Calculator")
     dpg.add_input_float(label="Tax Rate", width=100, tag=rate_id)
     dpg.add_input_float(label="Price", width=100, tag=price_id)
