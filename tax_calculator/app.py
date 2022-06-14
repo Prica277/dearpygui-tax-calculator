5b3121b092c9cf80d11d684035d0f2a3d88b8d80
prica277 <prica277@hsd.k12.or.us>
2022-06-14 11:22:04 -0700

    attempting completion

--- tax_calculator/app.py
+++ tax_calculator/app.py
@@ -6,19 +6,31 @@ rate_id = dpg.generate_uuid()
 price_id = dpg.generate_uuid()
 output_id = dpg.generate_uuid()
 
-def callback():
+def get_price():
     rate = dpg.get_value(rate_id)
     rate = round(rate, 3)
-    output= "Rate ID: " + str(rate)
+    price = dpg.get_value(price_id)
+    sales_tax = calculate_sales_tax(rate, price)
+    output= get_output(price, rate, sales_tax)
     dpg.set_value(output_id, output)
+
+def calculate_sales_tax(p, r):
+    #selling price x sales tax rate = sales tax, then selling price + sales tax = total price
+    return p + p * r
+
+def get_output(p, r, st):
+    output = "With a price of ${}".format(p)
+    output = "\nand a sales tax rate of {}".format(r)
+    output = "\nThe total price is ${}".format(st)
+    return output
 
 dpg.create_viewport(title='Sales Tax Calculator', width=600, height=300)
 
 with dpg.window(label="Sales Tax Calculator", width=600, height=300) :
     dpg.add_text("Welcome to the Sales Tax Calculator")
-    dpg.add_input_float(label="Tax Rate", width=100, tag=rate_id)
-    dpg.add_input_float(label="Price", width=100, tag=price_id)
-    dpg.add_button(label="Calculate", callback=callback)
+    dpg.add_input_float(label="Tax Rate", width=100, tag=rate_id, min_value=0, min_clamped=True, default_value=0)
+    dpg.add_input_float(label="Price", width=100, tag=price_id, min_value=0, min_clamped=True, default_value=0)
+    dpg.add_button(label="Calculate", callback=get_price)
     dpg.add_button(label="Clear")
     dpg.add_text("", tag=output_id)
 
--- tests/test_dearpygui_tax_calculator.py
+++ tests/test_dearpygui_tax_calculator.py
@@ -1,5 +1,11 @@
-from dearpygui_tax_calculator import __version__
+from tax_calculator import __version__
+from tax_calculator import app
 
 
 def test_version():
     assert __version__ == '0.1.0'
+
+def test_calculate_sales_tax():
+    expected = 104.0
+    results = app.calculate_sales_tax(100, 4)
+    assert expected == results
